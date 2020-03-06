7a07fd023a0dc2060c6b09aeb83b6708d7c48b89
SushainDilishan <sushaind96@gmail.com>
2020-03-06 15:48:12 +0530

    Created Administrator Service

---
+++ admninistratorService/src/main/java/com/Sushain/admninistratorService/AdmninistratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Sushain.admninistratorService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmninistratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmninistratorServiceApplication.class, args);
+	}
+
+}
---
+++ admninistratorService/src/test/java/com/Sushain/admninistratorService/AdmninistratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sushain.admninistratorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmninistratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
